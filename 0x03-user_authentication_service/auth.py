423371c2e22ee4f204f660e26179085403828d55
BezalelBenUri <diamondinyang@gmail.com>
2024-02-20 16:03:50 +0000

    5. Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,12 @@
 Module for authentication.
 """
 
+from uuid import uuid4
+
 import bcrypt
+
+from db import DB
+from user import User
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password and returns bytes.
@@ -24,3 +29,37 @@ def _generate_uuid() -> str:
         str: string representation of a new UUID.
     """
     return str(uuid4())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user with the given email and password.
+
+        Args:
+            email (str): The email of the new user.
+            password (str): The password of the new user.
+
+        Returns:
+            User: A User object representing the newly created user.
+
+        Raises:
+            ValueError: If a user with the given email already exists.
+        """
+        try:
+            # Search for the user by email
+            self._db.find_user_by(email=email)
+            # If a user already exist with the passed email, raise a ValueError
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+        # If not, hash the password with _hash_password
+        hashed_password = _hash_password(password)
+        # Save the user to the database using self._db
+        user = self._db.add_user(email, hashed_password)
+        # Return the User object
+        return user
